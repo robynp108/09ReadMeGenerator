7cdea377a8bc19ac397b7e62179513504ffbdd7c
robynp108 <robynp108@gmail.com>
2020-04-09 10:26:36 -0500

    api call, questions and first help session with TA

--- index.js
+++ index.js
@@ -1,12 +1,100 @@
-const questions = [
+const api = require("./utils/api");
+const inquirer = require("inquirer");
 
+const questions = [
+    {
+        type: "input",
+        message: "Enter your GitHub username:",
+        name: "username"
+    },
+    {
+        type: "checkbox",
+        name: "badge",
+        message: "Would you like a badge?",
+        choices: [
+            "Javascript 100%",
+            "Non-GMO",
+            "Gluten Free"
+        ]
+    },
+    {
+        type: "input",
+        name: "title",
+        message: "What is the project title?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "What is the project description?"
+    },
+    {
+        type: "checkbox",
+        name: "contents",
+        message: "What should be in the table of contents?",
+        choices: [
+            "* [Installation](#installation)",
+            "* [Usage](#usage)",
+            "* [Credits](#credits)",
+            "* [License](#license)"
+        ]
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "How is the project installed?"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "How is the project used?"
+    },
+    {
+        type: "input",
+        name: "license",
+        message: "How is the project licenced?"
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Are there any contributers?"
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "How was the project tested?"
+    },
 ];
 
 function writeToFile(fileName, data) {
+    inquirer.prompt(questions)
+        .then(function (data) {
+            //.then
+            init(data);
+            fileName = "README.md";
+
+            fs.writeFile(fileName, JSON.stringify(data, null, '\t'), function (err) {
+
+                if (err) {
+                    return console.log(err);
+                }
+
+
+            });
+        });
+
 }
 
-function init() {
+function init(userData) {
+    //to get the data we want from object
+    console.log(userData);
+    api.getUser().then(function (res) {
+        //make one variable of object with username, image, email.  Then return variable.
+        const image = res.data.avatar_url;
+        const email = res.data.email;
+        
+    });
 
 }
 
-init();
+
+writeToFile();
--- utils/api.js
+++ utils/api.js
@@ -1,7 +1,17 @@
+const fs = require("fs");
+const axios = require("axios");
+
 const api = {
   getUser(username) {
+        const queryUrl = `https://api.github.com/users/${username}`;
 
+        return axios.get(queryUrl)
+        // .then(function (res) {
+        //   const image = res.data.avatar_url;
+        //   const email = res.data.email;
+
+        // });
   }
-};
+}
 
 module.exports = api;
